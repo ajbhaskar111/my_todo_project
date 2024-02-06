ceef29df2773eb4e399261828c5ea23fdd4ae74f
ajbhaskar111 <ajbhaskar111@gmail.com>
2024-02-06 23:40:34 +0530

    create_model_and_migrate

--- project/todo_app/admin.py
+++ project/todo_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Role)
+admin.site.register(UserProfile)
+admin.site.register(ToDoList)
+admin.site.register(Department)
+admin.site.register(Master)
+admin.site.register(TaskAssociation)
--- project/todo_app/models.py
+++ project/todo_app/models.py
@@ -1,23 +1,45 @@
 from django.db import models
 
+class Role(models.Model):
+    Role = models.CharField(max_length=20)
+
+    class Meta:
+        db_table = 'role'
+
+    def __str__(self) -> str:
+        return self.Role
 
 # Create your models here.
-class master(models.Model):
+class Master(models.Model):
     Role = models.ForeignKey(Role, on_delete=models.CASCADE)
     Email = models.EmailField(unique=True)
-    Password = models.CharField(max_length = 25)
+    Password = models.CharField(max_length=25)
     IsActive = models.BooleanField(default=False)
 
+    class Meta:
+        db_table = 'master'
 
-gender_choise =(
+    def __str__(self) -> str:
+        return self.Email
+
+class Department(models.Model):
+    DeptName = models.CharField(max_length=25)
+
+    class Meta:
+        db_table = 'department'
+
+    def __str__(self) -> str:
+        return self.DeptName
+
+gender_choice = (
     ('m', 'male'),
     ('f', 'female')
 )
-class users(models.Model):
-    master = models.ForeignKey(master, on_delete=models.CASCADE)
+class UserProfile(models.Model):
+    master = models.ForeignKey(Master, on_delete=models.CASCADE)
     ProfileImage = models.FileField(upload_to='users/avatars', default='avatar.png')
     FullName = models.CharField(max_length = 35, blank=True, null=True)
-    Gender = models.CharField(max_length=2, choices=gender_choise)
+    Gender = models.CharField(max_length=2, choices= gender_choice)
     Dob = models.DateField()
     Phone = models.CharField(max_length = 10, unique = True, blank=True, null=True)
     Pincode = models.CharField(max_length= 5, blank=True, null=True)
@@ -32,5 +54,40 @@ class users(models.Model):
     Instagram = models.CharField(max_length = 35, blank=True, null=True)
     Website = models.CharField(max_length = 35, blank=True, null=True)
     Facebook = models.CharField(max_length = 35, blank=True, null=True)
-    
-    
+
+    class Meta:
+        db_table = 'userprofile'
+
+    def __str__(self) -> str:
+        return self.FullName
+
+status_choices = (
+    ('c', 'completed'),
+    ('p', 'pending'),
+    ('nt', 'not started')
+)
+class ToDoList(models.Model):
+    Title = models.CharField(max_length=25)
+    Tags = models.CharField(max_length=255)
+    Deadline = models.DateTimeField(auto_created=True)
+    DateCreated = models.DateTimeField(auto_now_add=True)
+
+    Description = models.TextField(max_length=255)
+    Status = models.CharField(max_length=15, choices=status_choices)
+    DateCompleted = models.DateTimeField(auto_created=True)
+
+    class Meta:
+        db_table = 'todolist'
+
+    def __str__(self) -> str:
+        return self.Title
+
+class TaskAssociation(models.Model):
+    Member = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    ToDoList = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'TaskAssociation'
+
+    def __str__(self) -> str:
+        return self.Member.FullName
